390823c9cc764041a9741620a975e691eeb212a3
giovanni.meroni <giovanni.meroni@polimi.it>
2019-02-07 15:11:29 +0100

    changed configfile path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Find the line host_ip: X.X.X.X and replace it with the docker host ip on the yml file
-sed -i -e 's/X.X.X.X/'$DOCKER_HOST_IP'/g' /app/config.json
+sed -i -e 's/X.X.X.X/'$DOCKER_HOST_IP'/g' app/config.json
 
 # Run the application
 npm start
